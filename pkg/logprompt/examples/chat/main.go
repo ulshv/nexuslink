7ac7f230a44977bf9ee392f5bfab5e06760eefd1
Sergey Cooper <hi@sergeycooper.com>
2025-01-30 17:17:29 +0300

    move logging/prompting to pkg/logprompt and add chat example

---
+++ pkg/logprompt/examples/chat/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/ulshv/nexuslink/pkg/logprompt"
+)
+
+var (
+	usernames = []string{"alice", "bob", "olivia", "peter", "sam", "sergey", "victor"}
+	messages  = []string{
+		"Hello, how are you?",
+		"I'm fine, thank you!",
+		"What's your name?",
+		"My name is anaonymous!",
+		"Nice to meet you, anaonymous!",
+	}
+)
+
+func randomUsername() string {
+	return usernames[rand.Intn(len(usernames))]
+}
+
+func randomMessage() string {
+	return messages[rand.Intn(len(messages))]
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	lp := logprompt.NewLogPrompt(context.Background(), "> ")
+
+	wg.Add(2)
+
+	go func() {
+		for {
+			lp.Log("[%s@0.0.0.0:5000]: %s", randomUsername(), randomMessage())
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		currUser := "admin"
+
+		for msg := range lp.Channel() {
+			lp.Log("[%s@0.0.0.0:5000]: %s", currUser, msg)
+		}
+	}()
+
+	lp.Start()
+}
--- pkg/logprompt/logprompt.go
+++ pkg/logprompt/logprompt.go
@@ -1,1 +1,108 @@
 package logprompt
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"unicode/utf8"
+
+	"golang.org/x/term"
+)
+
+const (
+	CLEAR_LINE = "\r\x1b[K" // Clear current terminal line
+)
+
+type LogPrompt struct {
+	prompt       string
+	currInput    string
+	isLastPrompt bool
+	ch           chan string
+	ctx          context.Context
+}
+
+func NewLogPrompt(ctx context.Context, prompt string) *LogPrompt {
+	return &LogPrompt{
+		prompt:       prompt,
+		currInput:    "",
+		isLastPrompt: false,
+		ch:           make(chan string),
+		ctx:          ctx,
+	}
+}
+
+func (lp *LogPrompt) Channel() <-chan string {
+	return lp.ch
+}
+
+func (lp *LogPrompt) Start() {
+	// Make stdin raw mode
+	oldTermState, err := makeTerminalRaw()
+	if err != nil {
+		fmt.Println("Failed to set raw mode:", err)
+		return
+	}
+	// Ensure we restore terminal state on exit
+	defer restoreTerminalState(oldTermState)
+	// Buffer for UTF-8/32 bit characters
+	buf := make([]byte, 4)
+	for {
+		// Read key strokes on terminal
+		n, err := os.Stdin.Read(buf)
+		char, _ := utf8.DecodeRune(buf[:n])
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		// Handle key strokes
+		switch char {
+		case 3, 4: // Ctrl+C, Ctrl+D
+			restoreTerminalState(oldTermState) // Restore terminal state before exiting
+			lp.Log("Exiting the program.")
+			os.Exit(0)
+		case '\n', 13: // Enter
+			lp.Log(lp.prompt + lp.currInput)
+			// send currInput to the channel
+			lp.ch <- lp.currInput
+			lp.currInput = ""
+			lp.printPromptLine()
+		case '\b', 127: // Backspace
+			if len(lp.currInput) > 0 {
+				lp.currInput = lp.currInput[:len(lp.currInput)-1]
+				lp.printPromptLine()
+			}
+		default:
+			lp.currInput += string(char)
+			lp.printPromptLine()
+		}
+	}
+}
+
+func (lp *LogPrompt) Stop() {
+	lp.ctx.Done()
+	// TODO: actually stop the loop started by Start()
+}
+
+func (lp *LogPrompt) Log(message string, args ...any) {
+	if lp.isLastPrompt {
+		fmt.Print(CLEAR_LINE)
+	}
+	fmt.Printf(message+"\n", args...)
+	lp.printPromptLine()
+}
+
+func (lp *LogPrompt) printPromptLine() {
+	fmt.Print(CLEAR_LINE)
+	fmt.Printf(lp.prompt + lp.currInput)
+	lp.isLastPrompt = true
+}
+
+// Helpers to make os.Stdin.Read() return every key stroke in the termanal:
+
+func makeTerminalRaw() (*term.State, error) {
+	return term.MakeRaw(int(os.Stdin.Fd()))
+}
+
+func restoreTerminalState(state *term.State) error {
+	return term.Restore(int(os.Stdin.Fd()), state)
+}
